3fab8c29167062c857c5d0e9feedb00cf73643ec
Oleksii Suriadnyi <Oleksii_Suriadnyi@epam.com>
2017-06-10 13:25:42 +0300

    Move start point to Global.asax (now Game.Instance starts by SpaceHub.OnDisconnected)

--- StarWars/Global.asax.cs
+++ StarWars/Global.asax.cs
@@ -1,30 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using Microsoft.AspNet.SignalR;
+using StarWars.Models;
 
 namespace StarWars
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-    public class MvcApplication : System.Web.HttpApplication
+  // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+  // visit http://go.microsoft.com/?LinkId=9394801
+  public class MvcApplication : System.Web.HttpApplication
+  {
+    protected void Application_Start()
     {
-        protected void Application_Start()
-        {
-            // Register the default hubs route: ~/signalr
-            RouteTable.Routes.MapHubs();
+      // Register the default hubs route: ~/signalr
+      RouteTable.Routes.MapHubs();
 
-            AreaRegistration.RegisterAllAreas();
+      AreaRegistration.RegisterAllAreas();
 
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+      FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+      BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-        }
+      Game.Instance.UpdateGameState(null);
     }
+  }
 }
