8fc12584d40113b0e3b832a1c1e2959d1f412b6a
Oleksii Suriadnyi <astrateg@yahoo.com>
2018-08-10 21:56:22 +0300

    Re-formatting indentation to make it consistent throghout C# classes.

--- StarWars.Web/Startup.cs
+++ StarWars.Web/Startup.cs
@@ -9,66 +9,66 @@ using StarWars.Web.Models;
 namespace StarWars.Web
 {
   public class Startup
+  {
+    public GameNotifier GameNotifier { get; private set; }
+
+    public Startup(IConfiguration configuration)
     {
-        public GameNotifier GameNotifier { get; private set; }
-
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddSignalR();
-            services.AddTransient<GameNotifier>();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime appLifetime)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
-
-            app.UseSignalR(routes =>
-            {
-                routes.MapHub<SpaceHub>("/spaceHub");
-            });
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-
-            appLifetime.ApplicationStarted.Register(() =>
-            {
-                GameNotifier = app.ApplicationServices.GetService<GameNotifier>();
-                GameNotifier.UpdateGameState(null);
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.Configure<CookiePolicyOptions>(options =>
+      {
+              // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+              options.CheckConsentNeeded = context => true;
+        options.MinimumSameSitePolicy = SameSiteMode.None;
+      });
+
+
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+      services.AddSignalR();
+      services.AddTransient<GameNotifier>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime appLifetime)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+      app.UseCookiePolicy();
+
+      app.UseSignalR(routes =>
+      {
+        routes.MapHub<SpaceHub>("/spaceHub");
+      });
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+                  name: "default",
+                  template: "{controller=Home}/{action=Index}/{id?}");
+      });
+
+      appLifetime.ApplicationStarted.Register(() =>
+      {
+        GameNotifier = app.ApplicationServices.GetService<GameNotifier>();
+        GameNotifier.UpdateGameState(null);
+      });
+    }
+  }
 }
